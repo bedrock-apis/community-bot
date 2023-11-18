ed9b0a3fe721affbd34e70cab818afb274ff1a7d
ConMaster2112 <samdembiny@gmail.com>
2023-11-18 21:58:54 +0100

    uuid updated

--- app/discord/commands/tools/uuid.ts
+++ app/discord/commands/tools/uuid.ts
@@ -12,11 +12,12 @@ export default {
             .setRequired(false)
         ),
     async execute(interaction: any) {
-        const count = interaction.options.getNumber("count",false)??1;
+        const count = Math.min(Math.abs(interaction.options.getNumber("count",false)??1),16);
         let obj = ``;
-        for (let i = 0; i < Math.min(Math.abs(count),16); i++) obj += `\n${uuidv4()}`;
+        for (let i = 0; i < count; i++) obj += `\n${uuidv4()}`;
+
         await interaction.reply({
-            embeds: [new EmbedBuilder().setColor(0x2b2d31).setTitle("UUID v1  -  " + Math.min(Math.abs(count),16) + "x").setDescription("```properties" + obj + "\n```")]
+            embeds: [new EmbedBuilder().setColor(0x2b2d31).setTitle(`UUID v4`).setDescription("UUID v4 is a version of UUID that is generated based on random numbers. It produces unique identifiers from separate applications without the coordination of a centralized agent or process."+"\n```properties" + obj + "\n```").setFooter({text:count + "x"})]
         });
     },
 } as CommandModuleDefinition;
