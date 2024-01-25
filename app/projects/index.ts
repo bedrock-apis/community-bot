ebcfca2d962bcdf355d9fdb5519f76da6b6397b2
ConMaster <64863757+conmaster2112@users.noreply.github.com>
2024-01-25 22:37:22 +0100

    NBT Experimental

--- app/projects/index.ts
+++ app/projects/index.ts
@@ -1,9 +1,12 @@
-import "./project-loader";
+import "./project-loader"; //Internal Project loader - other projects need this core
+
+
 import "./npm";
 import "./templates";
 import "./FAQ";
 
+// Experimental but Supported for now
+import "./NBT";
 //////////////////////// Developed -> please comment these one before pushing changes
 //import "./ts-compiler";
 //import "./diff";
-import "./NBT";
